d8fb76d523105baadf72696a4c2dce21cca003e9
Aubreychan <934913797@qq.com>
2022-07-13 01:48:27 +0800

    添加了双端队列的模板

---
+++ main/queue/Deque.go
@@ -0,0 +1,73 @@
+// 双端队列模板
+package main
+
+type Deque struct {
+	head *node
+	tail *node
+	size int
+}
+
+type node struct {
+	prev  *node
+	next  *node
+	value interface{}
+}
+
+func NewDeque() *Deque {
+	head := &node{nil, nil, nil}
+	tail := &node{nil, nil, nil}
+	head.next = tail
+	tail.prev = head
+	return &Deque{head, tail, 0}
+}
+
+func (this *Deque) Size() int {
+	return this.size
+}
+
+func (this *Deque) IsEmpty() bool {
+	return this.size == 0
+}
+
+func (this *Deque) Peek() interface{} {
+	if this.head == nil {
+		return nil
+	}
+	return this.head.value
+}
+
+func (this *Deque) PushFirst(v interface{}) {
+	val := &node{nil, nil, v}
+	last := this.head.next
+
+	this.head.next = val
+	val.next = last
+	val.prev = this.head
+	last.prev = val
+
+	this.size++
+}
+
+func (this *Deque) PushLast(v interface{}) {
+	val := &node{nil, nil, v}
+	front := this.tail.prev
+
+	front.next = val
+	val.prev = front
+
+	this.tail.prev = val
+	val.next = this.tail
+	this.size++
+}
+
+func (this *Deque) PollFirst() interface{} {
+	if this.size == 0 {
+		return nil
+	}
+
+	val := this.head.next
+	this.head.next = val.next
+	val.next.prev = this.head
+	this.size--
+	return val.value
+}
