b0a92ff7bc25ae6bcc3643720b53bf51288c16cf
Aubreychan <934913797@qq.com>
2022-08-03 17:15:04 +0800

    快速幂

---
+++ algorithm/dijkstra/dijkstra_heap.go
@@ -0,0 +1,10 @@
+package main
+
+const (
+	MAXVEX = 9 // 限制顶点个数，也可以不要
+	INF    = 0x3f3f3f3f
+)
+
+func dijkstra_heap(D [][]int, start int) {
+
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(0x100 | 0x001)
+
+}
